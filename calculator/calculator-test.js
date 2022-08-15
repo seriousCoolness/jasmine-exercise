8d4ee24fd2251227ccd7ea29e4531cd06605b999
seriousCoolness <wyattrobert42@gmail.com>
2022-08-15 04:39:09 -0400

    completed calculator

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,15 @@
 
 it('should calculate the monthly rate correctly', function () {
   // ...
+  expect(calculateMonthlyPayment({amount: 10.0, years: 1.0, rate: 1.0})).toEqual('$1.35');
+  expect(calculateMonthlyPayment({amount: 0, years: 1, rate: 1})).toEqual('$0.00');
+  expect(calculateMonthlyPayment({amount: 0, years: 0, rate: 0})).toEqual('$NaN');
 });
 
 
 it("should return a result with 2 decimal places", function() {
   // ..
+  expect(calculateMonthlyPayment({amount: 10.0, years: 1.0, rate: 1.0})).not.toEqual('$1.350');
 });
 
 /// etc
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -21,20 +21,39 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  let amount = document.getElementById("loan-amount");
+  let years = document.getElementById("loan-years");
+  let rate = document.getElementById("loan-rate");
+
+  amount.value = 10.0;
+  years.value = 1.0;
+  rate.value = 1.0;
+
+  update();
+
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  updateMonthly(calculateMonthlyPayment(getCurrentUIValues()));
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  
+  const interest = values.rate / 12.0;
+  const amount_of_payments = values.years * 12.0;
+  
+  const payment_raw_value = (values.amount * interest) / (1.0 - Math.pow(1.0 + interest,-amount_of_payments));
+
+  return '$' + payment_raw_value.toFixed(2);
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  document.getElementById("monthly-payment").innerText = monthly;
 }
